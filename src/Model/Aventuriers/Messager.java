c42786775f3dead038f1b856fbfcdb7f0fb40366
heyrendm <heyrendm@pc-dg-037-08>
2017-05-29 10:13:14 +0200

    initial

--- src/Model/Aventuriers/Messager.java
+++ src/Model/Aventuriers/Messager.java
@@ -7,9 +7,9 @@ public class Messager extends Aventurier {
 	 * @param aven1
 	 * @param aven2
 	 */
-	public void donnerCarte(Aventurier aven1, Aventurier aven2) {
+	public boolean donnerCartePossible(Aventurier aven1, Aventurier aven2) {
 		// TODO - implement Messager.donnerCarte
-		throw new UnsupportedOperationException();
+		return true;
 	}
 
 }
