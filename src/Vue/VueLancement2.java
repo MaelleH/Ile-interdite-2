3caead86d518f05349910afcac48fa7cfbb17167
heyrendm <heyrendm@pc-dg-027-02>
2017-06-20 14:08:00 +0200

    Merge origin/master
    Conflicts:
    	src/ile/interdite/Controleur.java

--- src/Vue/VueLancement2.java
+++ src/Vue/VueLancement2.java
@@ -5,23 +5,12 @@
  */
 package Vue;
 
-import ile.interdite.Message;
 import ile.interdite.Observateur;
-import static ile.interdite.TypeMessage.REGLES;
-import static ile.interdite.TypeMessage.VAL2;
 import java.awt.BorderLayout;
-import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.NORTH;
 import static java.awt.BorderLayout.SOUTH;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -30,10 +19,6 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -53,6 +38,9 @@ public class VueLancement2 {
     private JLabel mainPHaut;
     
     private JPanel welcomeP;
+    private JPanel choixP;
+    
+    
     private JPanel joueurP;
     private JPanel nomJoueurP;
     
@@ -68,20 +56,41 @@ public class VueLancement2 {
     public VueLancement2(Observateur obs) {
         controleur= obs;
         main = new JFrame();
+        //Creation du panel principal avec l'entete et le pied en image
         mainP = new JPanel(new BorderLayout());
         
-        ImageIcon tresorImage = new ImageIcon(System.getProperty("user.dir")+"/src/Vue/tresors.jpg");
+        PanelTresorBas tresorBas = new PanelTresorBas();
+        mainP.add(tresorBas,SOUTH);
         
-        mainPHaut = new JLabel((Icon) tresorImage);
-        mainPHaut.setSize(mainP.getWidth(),mainP.getHeight());
+        PanelIleHaut ileHaut = new PanelIleHaut();
+        mainP.add(ileHaut,NORTH);
         
-        mainPBas = new JPanel();
-        mainP.add(mainPHaut,NORTH);
-        mainP.add(mainPBas,SOUTH);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         welcomeP = new JPanel(new BorderLayout());
-        JPanel choixP = new JPanel(new BorderLayout());
+        choixP = new JPanel(new BorderLayout());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
@@ -100,162 +109,48 @@ public class VueLancement2 {
         
         
         //Fenetre principale
-        main.setTitle("Préparation du jeu ...");
+        main.setTitle("Bienvenue sur l'île interdite!");
         main.setSize(500, 500);
         
-        main.add(mainP);        
-        
-        
-        //Panneau de depart
-        
-        mainP.add(welcomeP,CENTER);        
-        welcomeP.add(choixP,CENTER); 
-        
-        //Titre 
-        JLabel bv= new JLabel("WELCOME", (int) JLabel.CENTER_ALIGNMENT);
-        welcomeP.add(bv,NORTH);
-    
-        
-        //Choix 
-        JPanel choixnbj = new JPanel();
-        choixP.add(choixnbj,NORTH);
-        //nbj 
-        choixnbj.add(new JLabel("Nombre de joueurs : "));
-        choixnbj.add(listeNiv);
-        
-        //Difficulté
-        JPanel choixDif = new JPanel();
-        choixP.add(choixDif,CENTER);
-        choixDif.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        choixDif.setBorder(BorderFactory.createTitledBorder("Difficulté"));
-        
-        // Déclaration de la combobox
-        choixDif.add(listeDif);
-        //bouton dif
-        
-        JButton val1 = new JButton("Valider Choix");
-
-        welcomeP.add(val1,SOUTH);
-
-
-        
-        //Panneau choix du nom des joueurs
-        joueurP = new JPanel(new BorderLayout());
-        nomJoueurP = new JPanel(new GridLayout(2,2));
-        joueurP.add(nomJoueurP,CENTER);
-        
-        
-        JPanel j1= new JPanel(new GridLayout(1, 2));j1.setVisible(false);
-        JPanel j2= new JPanel(new GridLayout(1, 2));j2.setVisible(false);
-        JPanel j3= new JPanel(new GridLayout(1, 2));j3.setVisible(false);
-        JPanel j4= new JPanel(new GridLayout(1, 2));j4.setVisible(false);
-        
-        nomJoueurP.add(j1);nomJoueurP.add(j2);nomJoueurP.add(j3);nomJoueurP.add(j4);
-        
-
-        
-        
-        j1.add(new JLabel("Joueur 1 : "));TextField nj1 = new TextField("Joaquim");j1.add(nj1);
-        j2.add(new JLabel("Joueur 2 : "));TextField nj2 = new TextField("Arnaud");j2.add(nj2);
-        j3.add(new JLabel("Joueur 3 : "));TextField nj3 = new TextField("Maëlle");j3.add(nj3);
-        j4.add(new JLabel("Joueur 4 : "));TextField nj4 = new TextField("Killian");j4.add(nj4);
-        
-        JButton val2 = new JButton("Valider le nom des joueurs");
-        
-        //Bouton pour valider les noms des joueurs et lancer le jeu
-        
-        
-        val2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Message m = new Message();
-                
-                switch (nbj[listeNiv.getSelectedIndex()]) {
-                    case "2":
-                        nomJ.add(nj1.getText());
-                        nomJ.add(nj2.getText());
-                        break;
-                    case "3":
-                        nomJ.add(nj1.getText());
-                        nomJ.add(nj2.getText());
-                        nomJ.add(nj3.getText());
-                        break;
-                    case "4":
-                        nomJ.add(nj1.getText());
-                        nomJ.add(nj2.getText());
-                        nomJ.add(nj3.getText());
-                        nomJ.add(nj4.getText());
-                        break;
-                    default:
-                        break;
-                }
-                
-                m.setNivDif(nivDif[listeDif.getSelectedIndex()]);
-                m.setJoueurs(nomJ);
-                m.setJoueur(nbj[listeNiv.getSelectedIndex()]);
-                m.setTypeMessage(VAL2);
-             
-                controleur.traiterMessage(m);
-                main.dispose();
-            }
-        });
-        JPanel valider= new JPanel(new GridLayout(1, 2));  
-        JPanel validerJ= new JPanel(new GridLayout(1, 3));        
-        joueurP.add(validerJ,SOUTH);
-        
-        
-        //Bouton regles
-        JButton regles = new JButton("Règles");
-        regles.addActionListener((ActionEvent e) -> {
-            Message m = new Message();
-            m.setTypeMessage(REGLES);  
-            controleur.traiterMessage(m);
-        });
-        valider.add(regles);
-        validerJ.add(regles);
-        
-        //Bouton RETOUR
-        JButton retour = new JButton("retour");
-        retour.addActionListener((ActionEvent e) -> {
-            mainP.remove(joueurP);
-            mainP.add(welcomeP);
-            mainP.repaint();
-            
-        });
-        
-        validerJ.add(retour);
-        validerJ.add(val2);
-        
-        //Bouton pour afficher les noms des joueurs
-        
-        
-        welcomeP.add(val1,SOUTH);    
-        
-        
-        
-                
-        val1.addActionListener((ActionEvent e) -> {
-            mainP.remove(welcomeP);
-            mainP.add(joueurP);
-            j1.setVisible(true);j2.setVisible(true);j4.setVisible(false);j3.setVisible(false);
-            if(nbj[listeNiv.getSelectedIndex()].equals("3")){
-                j3.setVisible(true);j4.setVisible(false);
-            }
-            else if(nbj[listeNiv.getSelectedIndex()].equals("4")){
-                j3.setVisible(true);j4.setVisible(true);
-            }
-            main.repaint();
-            
-        });
-        
-        
-        
-        
-        
-        
-        
+        main.add(mainP);      
         main.setVisible(true);
     }
 
+    public class PanelTresorBas extends JPanel{
+        
+        public PanelTresorBas(){
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+
+            try {
+                Image image = ImageIO.read(new File(System.getProperty("user.dir")+"/src/Vue/tresors.jpg"));
+                g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+            } catch (IOException ex) {
+                Logger.getLogger(PanelCarteTrophee.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+    }}
+     public class PanelIleHaut extends JPanel{
+        
+        public PanelIleHaut(){
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+
+            try {
+                Image image = ImageIO.read(new File(System.getProperty("user.dir")+"/src/Vue/ile.jpg"));
+                g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+            } catch (IOException ex) {
+                Logger.getLogger(PanelCarteTrophee.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+    }  
     
-}
+     }}
+
+
